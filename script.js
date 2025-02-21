dcaefb973f94f9973f782cdc852c51f92311f601
Suyog Khanal <khanalsuyog123@gmail.com>
2025-02-21 18:43:48 +0545

    eww

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const inputBox = document.getElementById('input-box')
 const addBtn = document.getElementById('add-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const doneBtn = document.getElementById('done-btn')
+const removeBtn = document.getElementById('remove-btn')
 let myTask = []
 var taskfromlocal = JSON.parse(localStorage.getItem("myTask"))
 console.log(taskfromlocal)
+
 if(taskfromlocal){
     myTask = taskfromlocal
     renderTask()
@@ -21,6 +23,7 @@ addBtn.addEventListener('click', ()=> {
     }
    inputBox.value=""
    localStorage.setItem("myTask",JSON.stringify(myTask))
+   taskfromlocal.appendchild(myTask)
   
 }   
 
@@ -28,7 +31,7 @@ addBtn.addEventListener('click', ()=> {
 function renderTask(){
     let taskList = ""
     for(let i=0; i<myTask.length;i++){
-        taskList += `<li>${i+1}${"."}${myTask[i]}</li>`
+        taskList += `<li>${i+1}${"."}${myTask[i]}<button class='delete' onclick='removeTask(this)'>X</button></li>`
     }
     list.innerHTML = taskList
     
@@ -38,4 +41,16 @@ deleteBtn.addEventListener('click', ()=>{
     myTask = []
     renderTask()
 
+})
+//dont btn have to edit more 
+doneBtn.addEventListener('click', ()=>{
+    const doneTask = myTask.splice(0,1)
+    myTask.push(doneTask)
+    renderTask()
+})
+//remove button have to edit more tmrw
+removeBtn.addEventListener('click', ()=>{
+    myTask.pop()
+    renderTask()
+    localStorage.removeItem('mytask')
 })
